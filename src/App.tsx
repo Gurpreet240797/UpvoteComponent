ab6ceea20861afc870b20ecdfc3af1e73667f68b
Gurpreet Singh <gurpreet24277@gmail.com>
2025-02-24 20:38:28 -0500

    Change Title

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const AppContent = () => {
   return (
     <div className="p-8 space-y-8"> 
       <div className="max-w-2/3 mx-auto space-y-4 border-2 border-gray-300 rounded-2xl p-4">
-        <h2 className="text-xl font-bold mt-8 mb-4">Using Store(Zustand)</h2>
+        <h2 className="text-xl font-bold mt-8 mb-4">Using Store</h2>
         <ZustandUpvoteList
           listId="list1"
           className="border-2 border-gray-200 rounded-xl p-6"
